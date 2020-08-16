0ee9f02dad7e95f8486dad0a6b6c01b7bf8f6044
Gisely Lucas Bernardino <gisely@ciandt.com>
2020-08-16 18:20:11 -0300

    Training

---
+++ page1/page1.js
@@ -0,0 +1,9 @@
+function onClick(element) {
+    element.classList.toggle('expanded');
+    var sibling = element.nextElementSibling;
+    sibling.classList.toggle('hidden');
+}
+
+function buttonForm() {
+    alert('lalala')
+}
